44a755edb822f0b050716f858306d49db2924a1d
amareswar253 <146627772+amareswar253@users.noreply.github.com>
2025-01-28 13:13:02 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,6 +3,7 @@ vpc_id = aws_vpc.abc.id
 tags = {
 Name = "subnet-1"
 }
+map_public_ip_on_launch = "true"
 availability_zone = "us-east-1a"
 cidr_block = "10.0.1.0/24"
 }
@@ -13,6 +14,7 @@ vpc_id = aws_vpc.abc.id
 tags = {
 Name = "subnet-2"
 }
+map_public_ip_on_launch = "true"
 availability_zone = "us-east-1b"
 cidr_block = "10.0.2.0/24"
 }
