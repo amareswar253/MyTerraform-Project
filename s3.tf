66f866ced720d3fe12e25468a10dca531b370042
amareswar253 <146627772+amareswar253@users.noreply.github.com>
2025-11-19 18:58:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -32,21 +32,4 @@ resource "aws_s3_bucket_public_access_block" "five" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "six" {
-  bucket = aws_s3_bucket.one.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "arn:aws:kms:us-east-1:445567096684:key/522da118-434e-45c3-8ce7-91ffd5770f74"  # Here need to give your aws KMS key as are presented in your AWS Account 
-    }
-  }
-}
-
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "amareswar.k8"
-key = "prod/terraform.tfstate"
-}
-}
